19c4af0e42abbb854c4db85157d358bb8a6f53ed
E. Handerson Obregon Simon <73681091+harrymanga@users.noreply.github.com>
2026-02-03 22:02:12 -0500

    Scripts

---
+++ Scripts_OS/Linux/Encabezado_de_Archivos_Sh/Plantilla.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###################################################################
+# Script Name   : backup.sh
+# Description   : Realiza una copia de seguridad de un directorio
+# Args          : $1 = directorio a copiar, $2 = ruta de destino
+# Author        : Juan Pérez
+# Email         : juan.perez@ejemplo.com
+# Date          : 2025-01-15
+###################################################################   
